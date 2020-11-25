7df0093dc679c24e0a6f1376718bda5a1359b929
Joao <joaolfelicio@gmail.com>
2020-11-25 06:11:33 +0100

    Add 'Is Number Unique' Solution with additional data structure
    Time Complexity: O(n)
    Space Complexity: O(n)

--- src/main/java/arraystring/_01_01_IsUnique.java
+++ src/main/java/arraystring/_01_01_IsUnique.java
@@ -1,13 +1,27 @@
 package arraystring;
 
+import java.util.HashSet;
+
 /**
  * Implement an algorithm to determine if a string has all unique characters.
+ * Page 90 - 1.1
  */
 
 class _01_01_IsUnique {
 
+    // With additional data structure
+    // Time Complexity: O(n)
+    // Space Complexity: O(n)
     boolean isUnique(String str) {
-        throw new UnsupportedOperationException();
+
+        HashSet<Character> chars = new HashSet<>();
+
+        for(int i = 0; i < str.length(); i++) {
+            if(chars.add(str.charAt(i)) == false) {
+                return false;
+            }
+        }
+        return true;
     }
 
 }
