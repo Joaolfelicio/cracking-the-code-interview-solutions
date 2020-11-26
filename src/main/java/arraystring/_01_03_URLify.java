bb5a42aeb8ad2171c1581688a9b1cd449402a9ec
Joao <joaolfelicio@gmail.com>
2020-11-26 06:57:05 +0100

    Add 'Urlify' Solution using Loop
    Time Complexity: O(n)
    Space Complexity: O(1)

--- src/main/java/arraystring/_01_03_URLify.java
+++ src/main/java/arraystring/_01_03_URLify.java
@@ -5,13 +5,37 @@ package arraystring;
  * You may assume that the string has sufficient space at the end to hold the additional characters,
  * and that you are given the "true" length of the string.
  * (Note: If implementing in Java,please use a character array so that you can perform this operation in place.)
- * <p>
+ *
  * EXAMPLE
  * Input:  "Mr John Smith    ", 13
  * Output: "Mr%20John%20Smith"
  */
+
 class _01_03_URLify {
     char[] urlify(char[] chars, int trueLength) {
-        throw new UnsupportedOperationException();
+        char[] escapeSpace = "%20".toCharArray();
+
+        int endPointer = chars.length - 1;
+        int startPointer = trueLength - 1;
+
+        while(startPointer >= 0) {
+
+            if(chars[startPointer] == ' ') {
+                for(int k = escapeSpace.length - 1; k >= 0; k--) {
+
+                    if(endPointer < startPointer || endPointer < 0) {
+                        throw new IllegalArgumentException();
+                    }
+
+                    chars[endPointer] = escapeSpace[k];
+                    endPointer--;
+                }
+            } else {
+                chars[endPointer] = chars[startPointer];
+                endPointer--;
+            }
+            startPointer--;
+        }
+        return chars;
     }
 }
--- src/test/java/arraystring/_01_03_URLifyTest.java
+++ src/test/java/arraystring/_01_03_URLifyTest.java
@@ -9,22 +9,24 @@ public class _01_03_URLifyTest {
     private _01_03_URLify s = new _01_03_URLify();
 
     @Test
-    public void withNoSpace() {
-        assertArrayEquals("hello".toCharArray(), s.urlify("hello".toCharArray(), 5));
-    }
+    public void defaultTest() { assertArrayEquals("Mr%20John%20Smith".toCharArray(), s.urlify("Mr John Smith    ".toCharArray(), 13)); }
+
+    @Test
+    public void withNoSpace() { assertArrayEquals("hello".toCharArray(), s.urlify("hello".toCharArray(), 5)); }
 
     @Test(expected = IllegalArgumentException.class)
     public void withInsufficientLength() {
         s.urlify("hello world ".toCharArray(), 11);
     }
 
-    @Test
-    public void withOneSpace() {
-        assertArrayEquals("hello%20world".toCharArray(), s.urlify("hello world  ".toCharArray(), 11));
+    @Test(expected = IllegalArgumentException.class)
+    public void withInsufficientLengthInTheBeginning() {
+        s.urlify(" helloworld ".toCharArray(), 11);
     }
 
     @Test
-    public void withTwoSpaces() {
-        assertArrayEquals("hello%20world%20bob".toCharArray(), s.urlify("hello world bob    ".toCharArray(), 15));
-    }
+    public void withOneSpace() { assertArrayEquals("hello%20world".toCharArray(), s.urlify("hello world  ".toCharArray(), 11)); }
+
+    @Test
+    public void withTwoSpaces() { assertArrayEquals("hello%20world%20bob".toCharArray(), s.urlify("hello world bob    ".toCharArray(), 15)); }
 }
