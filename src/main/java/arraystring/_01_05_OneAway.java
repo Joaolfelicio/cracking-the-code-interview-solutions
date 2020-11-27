7becd6ae90519f438d0a5eeddc28a0846fda460d
Joao <joaolfelicio@gmail.com>
2020-11-27 07:21:14 +0100

    Add 'One Away' Solution, need to refactor
    Time Complexity: O(n)
    Space Complexity: O(1)

--- src/main/java/arraystring/_01_05_OneAway.java
+++ src/main/java/arraystring/_01_05_OneAway.java
@@ -6,14 +6,47 @@ package arraystring;
  * Given two strings, write a function to check if they are one edit (or zero edits) away.
  * <p>
  * EXAMPLE
- * pale, ple -> true
- * pales, pale -> true
- * pale, bale -> true
- * pale, bake -> false
+ * pale,  ple   -> true
+ * pales, pale  -> true
+ * pale,  bale  -> true
+ * pale,  bake  -> false
+ * pae,   pale  -> true
+ * paxye, paxe -> false
  */
 class _01_05_OneAway {
     boolean isOneAway(String a, String b) {
-        throw new UnsupportedOperationException();
+
+        String bigStr = a.length() > b.length() ? a : b;
+        String smallStr = a.length() > b.length() ? b : a;
+
+        if(a.length() + 1 < b.length() || b.length() + 1 < a.length()) return false;
+
+        int numOfChanges = 0;
+        int bigArrPointer = bigStr.length() - 1;
+        int smallArrPointer = smallStr.length() - 1;
+
+        // Change
+        if(a.length() == b.length()) {
+            while(bigArrPointer >= 0) {
+                if(bigStr.charAt(bigArrPointer) != smallStr.charAt(bigArrPointer)) {
+                    numOfChanges++;
+                }
+                bigArrPointer--;
+            }
+
+        // Remove and Add
+        } else {
+            while(bigArrPointer >= 0 && smallArrPointer >= 0) {
+                if (bigStr.charAt(bigArrPointer) == smallStr.charAt(smallArrPointer)) {
+                    smallArrPointer--;
+                } else {
+                    numOfChanges++;
+                }
+                bigArrPointer--;
+            }
+        }
+
+        return numOfChanges < 2;
     }
 
 }
