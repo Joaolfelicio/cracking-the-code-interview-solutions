4271d9ff95df8443f486597bb5e188df7f675ffe
Joao <joaolfelicio@gmail.com>
2020-11-29 16:22:32 +0100

    Add 'Rotate Matrix' Solution

--- src/main/java/arraystring/_01_07_RotateMatrix.java
+++ src/main/java/arraystring/_01_07_RotateMatrix.java
@@ -4,10 +4,28 @@ package arraystring;
  * Given an image represented by an NxN matrix, where each pixel in the image is 4 bytes,
  * write a method to rotate the image by 90 degrees. Can you do this in place?
  */
+
 class _01_07_RotateMatrix {
     int[][] rotate(int[][] m) {
-        throw new UnsupportedOperationException();
+        int n = m.length;
+
+        if(n == 0) return m;
+
+        for(int i = 0; i < n; i++) {
+            for(int j = i; j < n; j++) {
+                int temp = m[i][j];
+                m[i][j] = m[j][i];
+                m[j][i] = temp;
+            }
+        }
+
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n/2; j++) {
+                int temp = m[i][n - 1 - j];
+                m[i][n - 1 - j] = m[i][j];
+                m[i][j] = temp;
+            }
+        }
+        return m;
     }
-
-
 }
