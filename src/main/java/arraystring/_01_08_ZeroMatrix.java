df705f38e7de9e346a26a2e50bb775aee4e79d0d
Joao <joaolfelicio@gmail.com>
2020-11-29 18:08:52 +0100

    Add 'Zero Matrix', using additional storage
    Need to refactor so the space complexity is O(1)

--- src/main/java/arraystring/_01_08_ZeroMatrix.java
+++ src/main/java/arraystring/_01_08_ZeroMatrix.java
@@ -5,6 +5,30 @@ package arraystring;
  */
 class _01_08_ZeroMatrix {
     int[][] zero(int[][] matrix) {
-        throw new UnsupportedOperationException();
+        boolean[][] zeroedArr = new boolean[matrix.length][matrix[0].length];
+
+        for(int row = 0; row < matrix.length; row++) {
+            for(int col = 0; col < matrix[row].length; col++) {
+                if(matrix[row][col] == 0 && !zeroedArr[row][col]) {
+                    zeroRowColumn(matrix, zeroedArr, row, col);
+                }
+            }
+        }
+        return matrix;
+    }
+
+    void zeroRowColumn(int[][] matrix, boolean[][] zeroedArr, int row, int col) {
+        for(int i = 0; i < matrix[row].length; i++) {
+            if(matrix[row][i] != 0) {
+                matrix[row][i] = 0;
+                zeroedArr[row][i] = true;
+            }
+        }
+        for(int i = 0; i < matrix.length; i++) {
+            if(matrix[i][col] != 0) {
+                matrix[i][col] = 0;
+                zeroedArr[i][col] = true;
+            }
+        }
     }
 }
