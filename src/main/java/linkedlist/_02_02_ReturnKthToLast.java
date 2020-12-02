b23dbc3850f2499be98d13d0dd3251c5146eadd9
Joao <joaolfelicio@gmail.com>
2020-12-02 06:43:31 +0100

    Add 'Return Kth To Last Elem In Linked List' Solution

--- src/main/java/linkedlist/_02_02_ReturnKthToLast.java
+++ src/main/java/linkedlist/_02_02_ReturnKthToLast.java
@@ -6,6 +6,24 @@ package linkedlist;
 class _02_02_ReturnKthToLast {
 
     LinkedListNode kthToLast(LinkedListNode head, int k) {
-        throw new UnsupportedOperationException();
+        if(head == null) return null;
+
+        LinkedListNode curr = head;
+        int length = 0;
+
+        while(curr != null) {
+            length++;
+            curr = curr.next;
+        }
+
+        if(k > length - 1 || k < 0) return null;
+
+        curr = head;
+        int currPos = 0;
+        while(currPos != length - 1 - k) {
+            currPos++;
+            curr = curr.next;
+        }
+        return curr;
     }
 }
