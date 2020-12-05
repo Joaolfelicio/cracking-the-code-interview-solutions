b7c663fed78ffcf9f1e3e934ba06356defc09909
Joao <joaolfelicio@gmail.com>
2020-12-05 17:24:05 +0100

    Refactor 'Sum Linked List' Solution to one single pass

--- src/main/java/linkedlist/_02_05_SumList.java
+++ src/main/java/linkedlist/_02_05_SumList.java
@@ -20,30 +20,39 @@ class _02_05_SumList {
 
         LinkedListNode currL1 = l1;
         LinkedListNode currL2 = l2;
-
-        int sum = sumLinkedList(currL2) + sumLinkedList(currL1);
-
         LinkedListNode dummyList = new LinkedListNode(0);
         LinkedListNode curr = dummyList;
 
-        while(sum > 0) {
-            curr.next = new LinkedListNode(sum % 10);
-            sum /= 10;
+        int carry = 0;
+
+        while(currL1 != null || currL2 != null) {
+
+            int valL1 = 0;
+            int valL2 = 0;
+
+            if(currL1 != null) {
+                valL1 = currL1.val;
+                currL1 = currL1.next;
+            }
+
+            if(currL2 != null) {
+                valL2 = currL2.val;
+                currL2 = currL2.next;
+            }
+
+            int val = valL1 + valL2 + carry;
+
+            carry = 0;
+
+            if(val > 9) carry = 1;
+
+            curr.next = new LinkedListNode(val % 10);
+
             curr = curr.next;
         }
 
+        if(carry > 0) curr.next = new LinkedListNode(carry);
+
         return dummyList.next;
-    }
-
-    int sumLinkedList(LinkedListNode node) {
-        int multiplier = 1;
-        int total = 0;
-
-        while (node != null) {
-            total += node.val * multiplier;
-            multiplier *= 10;
-            node = node.next;
-        }
-        return total;
     }
 }
--- src/test/java/linkedlist/_02_05_SumListTest.java
+++ src/test/java/linkedlist/_02_05_SumListTest.java
@@ -20,26 +20,36 @@ public class _02_05_SumListTest {
 
     @Test
     public void withNoCarry() {
-        assertEquals(LinkedListNode.of(2, 4, 6), s.sum(LinkedListNode.of(1, 2, 3), LinkedListNode.of(1, 2, 3)));
+        assertEquals(LinkedListNode.of(6, 4, 2), s.sum(LinkedListNode.of(3, 2, 1), LinkedListNode.of(3, 2, 1)));
     }
 
     @Test
     public void withCarry() {
-        assertEquals(LinkedListNode.of(5, 3, 5), s.sum(LinkedListNode.of(1, 8, 3), LinkedListNode.of(3, 5, 2)));
+        assertEquals(LinkedListNode.of(5, 3, 5), s.sum(LinkedListNode.of(3, 8, 1), LinkedListNode.of(2, 5, 3)));
     }
 
     @Test
     public void withCarryAtEnd() {
-        assertEquals(LinkedListNode.of(1, 1, 4, 1), s.sum(LinkedListNode.of(1, 8, 9), LinkedListNode.of(9, 5, 2)));
+        assertEquals(LinkedListNode.of(1, 4, 1, 1), s.sum(LinkedListNode.of(9, 8, 1), LinkedListNode.of(2, 5, 9)));
     }
 
     @Test
     public void withTwoListsHavingDiffrentLength() {
-        assertEquals(LinkedListNode.of(2, 4, 1), s.sum(LinkedListNode.of(1, 8, 9), LinkedListNode.of(5, 2)));
+        assertEquals(LinkedListNode.of(1, 4, 2), s.sum(LinkedListNode.of(9, 8, 1), LinkedListNode.of(2, 5)));
     }
 
     @Test
     public void withProvidedExample() {
-        assertEquals(LinkedListNode.of(2, 1, 9), s.sum(LinkedListNode.of(7,1 , 6), LinkedListNode.of(5, 9, 2)));
+        assertEquals(LinkedListNode.of(2, 1, 9), s.sum(LinkedListNode.of(7, 1, 6), LinkedListNode.of(5, 9, 2)));
+    }
+
+    @Test
+    public void withProvidedExampleAndCarry() {
+        assertEquals(LinkedListNode.of(2, 1, 1, 1), s.sum(LinkedListNode.of(7, 1, 6), LinkedListNode.of(5, 9, 4)));
+    }
+
+    @Test
+    public void withProvidedExampleDifferentQuantityDigits() {
+        assertEquals(LinkedListNode.of(2, 6, 7, 7), s.sum(LinkedListNode.of(8, 6, 1, 7), LinkedListNode.of(4, 9, 5)));
     }
 }
