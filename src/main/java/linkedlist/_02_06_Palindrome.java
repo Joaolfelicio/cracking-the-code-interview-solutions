dcf64b0dc66cb715ddb5012e8233e7b6dfdacfe4
Joao <joaolfelicio@gmail.com>
2020-12-05 20:48:39 +0100

    Add 'Palindrome Linked List', want to improve space complexity

--- src/main/java/linkedlist/_02_06_Palindrome.java
+++ src/main/java/linkedlist/_02_06_Palindrome.java
@@ -1,12 +1,39 @@
 package linkedlist;
 
+import java.util.Stack;
+
 /**
  * Palindrome: Implement a function to check if a linked list is a palindrome.
  */
 class _02_06_Palindrome {
 
     boolean isPalindrome(LinkedListNode head) {
-        throw new UnsupportedOperationException();
+        if(head == null || head.next == null) return true;
+
+        Stack<Integer> stack = new Stack<>();
+
+        LinkedListNode slow = head;
+        LinkedListNode fast = head.next.next;
+        
+        while (fast != null && fast.next != null) {
+            stack.push(slow.val);
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        stack.push(slow.val);
+
+        if(fast == null)
+            slow = slow.next;
+        else if(fast.next == null)
+            slow = slow.next.next;
+
+        while(slow != null) {
+            if(slow.val != stack.pop()) return false;
+            slow = slow.next;
+        }
+
+        return stack.isEmpty();
     }
 
 
