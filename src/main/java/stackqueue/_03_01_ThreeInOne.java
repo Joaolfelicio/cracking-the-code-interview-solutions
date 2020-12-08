722619424214981abeffb4ec4ff7d3e948cf4d62
Joao <joaolfelicio@gmail.com>
2020-12-08 19:36:25 +0100

    Add 'Three Stack in One Array' Solution using fixed space

--- src/main/java/stackqueue/_03_01_ThreeInOne.java
+++ src/main/java/stackqueue/_03_01_ThreeInOne.java
@@ -5,18 +5,52 @@ package stackqueue;
  */
 class _03_01_ThreeInOne {
 
+    int sizePerStack;
+    int[] tripleStack;
+    int[] stackTails;
+    int stackQuantity = 3;
+
 
     _03_01_ThreeInOne(int initSize) {
-
+        tripleStack = new int[initSize * stackQuantity];
+        stackTails = new int[stackQuantity];
+        sizePerStack = initSize;
     }
 
-    void push(int stackNum, int val) {
-        throw new UnsupportedOperationException();
+    void push(int stackNum, int val) throws Exception {
+
+        if(stackNum >= 0 && stackNum < stackQuantity) {
+            pushToStack(stackNum, val);
+        } else {
+            throw new RuntimeException("Invalid stack number");
+        }
     }
 
-    int pop(int stackNum) {
-        throw new UnsupportedOperationException();
+    private void pushToStack(int stackNum, int val) throws Exception {
+        if(stackTails[stackNum] == sizePerStack) throw new RuntimeException("Stack " + stackNum + " is already full");
+
+        int newTail = sizePerStack * stackNum + stackTails[stackNum];
+
+        tripleStack[newTail] = val;
+
+        stackTails[stackNum]++;
     }
 
+    int pop(int stackNum) throws Exception {
+        if(stackNum >= 0 && stackNum < stackQuantity) {
+            return popFromStack(stackNum);
+        } else {
+            throw new RuntimeException("Invalid stack number");
+        }
+    }
 
+    private int popFromStack(int stackNum) throws Exception {
+        if(stackTails[stackNum] == 0) throw new RuntimeException("Stack " + stackNum + " is empty");
+
+        int indexValue = sizePerStack * stackNum + stackTails[stackNum];
+
+        stackTails[stackNum]--;
+
+        return tripleStack[indexValue - 1];
+    }
 }
--- src/test/java/stackqueue/_03_01_ThreeInOneTest.java
+++ src/test/java/stackqueue/_03_01_ThreeInOneTest.java
@@ -7,7 +7,7 @@ import static org.junit.Assert.assertEquals;
 public class _03_01_ThreeInOneTest {
 
     @Test
-    public void withStackSize1() {
+    public void withStackSize1() throws Exception {
         _03_01_ThreeInOne s = new _03_01_ThreeInOne(1);
         s.push(0, 0);
         s.push(1, 1);
@@ -19,24 +19,22 @@ public class _03_01_ThreeInOneTest {
 
 
     @Test
-    public void withStackSize2() {
+    public void withStackSize2() throws Exception {
         _03_01_ThreeInOne s = new _03_01_ThreeInOne(2);
         s.push(0, 6);
         s.push(1, 1);
         s.push(1, 2);
         s.push(2, 3);
         s.push(2, 4);
-        s.push(2, 5);
         assertEquals(6, s.pop(0));
         assertEquals(2, s.pop(1));
         assertEquals(1, s.pop(1));
-        assertEquals(5, s.pop(2));
         assertEquals(4, s.pop(2));
         assertEquals(3, s.pop(2));
     }
 
     @Test(expected = RuntimeException.class)
-    public void withArrayLength3_Exceeded() {
+    public void withArrayLength3_Exceeded() throws Exception {
         _03_01_ThreeInOne s = new _03_01_ThreeInOne(1);
         s.push(0, 1);
         s.push(0, 1);
@@ -45,7 +43,7 @@ public class _03_01_ThreeInOneTest {
     }
 
     @Test(expected = RuntimeException.class)
-    public void withArrayLength3_StackNotExists() {
+    public void withArrayLength3_StackNotExists() throws Exception {
         _03_01_ThreeInOne s = new _03_01_ThreeInOne(3);
         s.push(3, 1);
     }
