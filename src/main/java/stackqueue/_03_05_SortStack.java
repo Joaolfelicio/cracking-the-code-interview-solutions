2e3e643398dfe2d427b08fa2206a633305b7b6bc
Joao <joaolfelicio@gmail.com>
2020-12-10 19:33:51 +0100

    Add 'Sort Stack' solution

--- src/main/java/stackqueue/_03_05_SortStack.java
+++ src/main/java/stackqueue/_03_05_SortStack.java
@@ -8,9 +8,43 @@ import java.util.Stack;
  * but you may not copy the elements into any other data structure (such as an array).
  * The stack supports the following operations: push, pop, peek, and isEmpty.
  */
-class _03_05_SortStack {
+class  _03_05_SortStack {
 
-    Stack<Integer> sort(Stack<Integer> stack) {
-        throw new UnsupportedOperationException();
+    Stack<Integer> sort(Stack<Integer> hiValuesOnTopStack) {
+        Stack<Integer> loValuesOnTopStack = new Stack<>();
+
+        int counter = 0;
+
+        while(counter != hiValuesOnTopStack.size()) {
+
+            // Kee
+            counter = 0;
+
+            while(!hiValuesOnTopStack.isEmpty()){
+                int valToCompare = hiValuesOnTopStack.pop();
+
+                if (hiValuesOnTopStack.isEmpty() || valToCompare >= hiValuesOnTopStack.peek()) {
+                    loValuesOnTopStack.push(valToCompare);
+                    counter++;
+                } else {
+                    loValuesOnTopStack.push(hiValuesOnTopStack.pop());
+                    hiValuesOnTopStack.push(valToCompare);
+                }
+            }
+
+            if(counter == loValuesOnTopStack.size()) break;
+
+            while(!loValuesOnTopStack.isEmpty()){
+                int valToCompare = loValuesOnTopStack.pop();
+
+                if (loValuesOnTopStack.isEmpty() || valToCompare <= loValuesOnTopStack.peek() ) {
+                    hiValuesOnTopStack.push(valToCompare);
+                } else {
+                    hiValuesOnTopStack.push(loValuesOnTopStack.pop());
+                    loValuesOnTopStack.push(valToCompare);
+                }
+            }
+        }
+        return loValuesOnTopStack;
     }
 }
