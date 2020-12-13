690fd09009048a80f7390387028b4314ef772e2f
Joao <joaolfelicio@gmail.com>
2020-12-13 13:23:35 +0100

    Add 'List Of Depths' Solution using BFS

--- src/main/java/treegraph/_04_03_ListOfDepths.java
+++ src/main/java/treegraph/_04_03_ListOfDepths.java
@@ -1,13 +1,43 @@
 package treegraph;
 
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 class _04_03_ListOfDepths {
 
     //time o(N) space o(N)
     List<LinkedList<Integer>> create(BinaryTreeNode node) {
-        throw new UnsupportedOperationException();
-    }
+        LinkedList<LinkedList<Integer>> depthLists = new LinkedList<>();
 
+        if(node == null) return depthLists;
+
+        Queue<BinaryTreeNode> treeNodeQueue = new LinkedList();
+
+        treeNodeQueue.add(node);
+
+        while (!treeNodeQueue.isEmpty()) {
+
+            LinkedList<Integer> depth = new LinkedList<>();
+            int queueSize = treeNodeQueue.size();
+
+            for(int i = 0; i < queueSize; i++) {
+                BinaryTreeNode currNode = treeNodeQueue.poll();
+
+                if(currNode.left != null) {
+                    treeNodeQueue.add(currNode.left);
+                }
+                if(currNode.right != null) {
+                    treeNodeQueue.add(currNode.right);
+                }
+
+                depth.add(currNode.val);
+            }
+
+            depthLists.add(depth);
+        }
+
+        return depthLists;
+    }
 }
