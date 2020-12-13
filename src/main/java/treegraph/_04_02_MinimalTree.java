de7d9c6738d47f34d93be6515ac28495a457afea
Joao <joaolfelicio@gmail.com>
2020-12-13 11:53:10 +0100

    Add 'Minimal Tree' Solution

--- src/main/java/treegraph/_04_02_MinimalTree.java
+++ src/main/java/treegraph/_04_02_MinimalTree.java
@@ -7,8 +7,21 @@ package treegraph;
 class _04_02_MinimalTree {
 
     BinaryTreeNode buildMinimalTree(int[] vals) {
-        throw new UnsupportedOperationException();
+        if(vals.length == 0) return null;
+
+        return buildTree(vals, 0, vals.length - 1);
     }
 
+    BinaryTreeNode buildTree(int[] vals, int p1, int p2) {
+        if(p1 > p2) return null;
 
+        int midIndex = p1 + (p2 - p1) / 2;
+
+        BinaryTreeNode node = new BinaryTreeNode(vals[midIndex]);
+
+        node.left = buildTree(vals, p1, midIndex - 1);
+        node.right = buildTree(vals, midIndex + 1, p2);
+
+        return node;
+    }
 }
