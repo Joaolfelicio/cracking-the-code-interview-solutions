a07982b1ee41845c243f28fdffbe41d4feaa0ad1
Joao <joaolfelicio@gmail.com>
2020-12-16 18:02:59 +0100

    Add 'Find Common Ancestor' Brute Force Solution

--- src/main/java/treegraph/_04_08_FindCommonAncestor.java
+++ src/main/java/treegraph/_04_08_FindCommonAncestor.java
@@ -1,5 +1,7 @@
 package treegraph;
 
+import java.util.HashSet;
+
 /**
  * Design an algorithm and write code to find the first common ancestor of two nodes in a binary tree.
  * Avoid storing additional nodes in a data structure. NOTE: This is not necessarily a binary search tree.
@@ -7,7 +9,23 @@ package treegraph;
 class _04_08_FindCommonAncestor {
 
     ParentAwareBinaryTreeNode findCommonAncestor(ParentAwareBinaryTreeNode a, ParentAwareBinaryTreeNode b) {
-        throw new UnsupportedOperationException();
+        if(a == null || b == null) return null;
+
+        ParentAwareBinaryTreeNode currA = a;
+        ParentAwareBinaryTreeNode currB = b;
+        HashSet<ParentAwareBinaryTreeNode> ancestorsB = new HashSet<>();
+
+        while(currB != null) {
+            ancestorsB.add(currB);
+            currB = currB.parent;
+        }
+
+        while(currA != null) {
+            if(ancestorsB.contains(currA)) return currA;
+            currA = currA.parent;
+        }
+
+        return null;
     }
 
 
