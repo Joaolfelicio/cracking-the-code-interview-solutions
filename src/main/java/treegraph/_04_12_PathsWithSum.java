3f7ba25fa61a2e5cb68bb03493242f10fabef221
Joao <joaolfelicio@gmail.com>
2020-12-23 18:19:37 +0100

    Add 'Count Path With Sum' Solution

--- src/main/java/treegraph/_04_12_PathsWithSum.java
+++ src/main/java/treegraph/_04_12_PathsWithSum.java
@@ -1,5 +1,7 @@
 package treegraph;
 
+import java.util.HashMap;
+
 /**
  * You are given a binary tree in which each node contains an integer value (which might be positive or negative).
  * Design an algorithm to count the number of paths that target to a given value.
@@ -8,11 +10,21 @@ package treegraph;
  */
 class _04_12_PathsWithSum {
 
-
-
     int countPathWithSum(BinaryTreeNode node, int sum) {
-        throw new UnsupportedOperationException();
+        HashMap<Integer, Integer> preSum = new HashMap();
+        preSum.put(0,1);
+        return helper(node, 0, sum, preSum);
     }
 
+    private int helper(BinaryTreeNode root, int currSum, int target, HashMap<Integer, Integer> preSum) {
+        if (root == null) return 0;
 
+        currSum += root.val;
+        int res = preSum.getOrDefault(currSum - target, 0);
+        preSum.put(currSum, preSum.getOrDefault(currSum, 0) + 1);
+
+        res += helper(root.left, currSum, target, preSum) + helper(root.right, currSum, target, preSum);
+        preSum.put(currSum, preSum.get(currSum) - 1);
+        return res;
+    }
 }
