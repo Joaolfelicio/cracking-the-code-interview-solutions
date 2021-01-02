7173d90a17a7e9b4c4d8fbe6f0b2e815866a796b
Joao <joaolfelicio@gmail.com>
2021-01-02 16:42:25 +0100

    Add Circular Array Design

---
+++ Object Oriented Design/CircularArray/CircularArray/CircularArray.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace CircularArray
+{
+    public class CircularArray<T>
+    {
+        public T[] Items { get; set; }
+        private int Head = 0;
+
+        public CircularArray(int size)
+        {
+            Items = new T[size];
+        }
+
+        private int Convert(int index)
+        {
+            if (index < 0) index += Items.Length;
+            return (Head + index) % Items.Length;
+        }
+
+        public void Rotate(int shiftRight) => Head = Convert(shiftRight);
+
+        public T Get(int index)
+        {
+            if( index < 0 || index >= Items.Length) throw new IndexOutOfRangeException();
+
+            return Items[Convert(index)];
+        }
+
+        public void Set(int index, T item) => Items[Convert(index)] = item;
+    }
+}
