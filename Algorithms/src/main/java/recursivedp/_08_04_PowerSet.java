9e109f50529ffce8ff8ffe23c8ad39979f0b3e8b
Joao <joaolfelicio@gmail.com>
2021-01-05 19:26:36 +0100

    Add Power Set Solution

--- Algorithms/src/main/java/recursivedp/_08_04_PowerSet.java
+++ Algorithms/src/main/java/recursivedp/_08_04_PowerSet.java
@@ -1,5 +1,6 @@
 package recursivedp;
 
+import java.util.ArrayList;
 import java.util.Set;
 
 /**
@@ -7,8 +8,26 @@ import java.util.Set;
  */
 class _08_04_PowerSet {
 
-    Set<Set<Integer>> subSets(Set<Integer> set) {
-        throw new UnsupportedOperationException();
+    ArrayList<ArrayList<Integer>> subSets(ArrayList<Integer> set, int index) {
+        ArrayList<ArrayList<Integer>> subsets;
+
+        if(set.size() == index) {
+            subsets = new ArrayList<ArrayList<Integer>>();
+            subsets.add(new ArrayList<Integer>());
+        } else {
+            subsets = subSets(set, index + 1);
+            var item = set.get(index);
+            var moreSubsets = new ArrayList<ArrayList<Integer>>();
+
+            for(ArrayList<Integer> subset : subsets) {
+                var newSubset = new ArrayList<Integer>();
+                newSubset.addAll(subset);
+                newSubset.add(item);
+                moreSubsets.add(newSubset);
+            }
+            subsets.addAll(moreSubsets);
+        }
+        return subsets;
     }
 
 
