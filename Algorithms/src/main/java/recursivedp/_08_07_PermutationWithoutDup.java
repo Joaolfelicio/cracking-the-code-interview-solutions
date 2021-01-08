1901ac7fe0dc1cc799dc65267e29e6f8ab9db690
Joao <joaolfelicio@gmail.com>
2021-01-08 17:58:36 +0100

    Add Permutation without Duplicates Solution

--- Algorithms/src/main/java/recursivedp/_08_07_PermutationWithoutDup.java
+++ Algorithms/src/main/java/recursivedp/_08_07_PermutationWithoutDup.java
@@ -1,5 +1,6 @@
 package recursivedp;
 
+import java.util.ArrayList;
 import java.util.Set;
 
 /**
@@ -7,8 +8,34 @@ import java.util.Set;
  */
 class _08_07_PermutationWithoutDup {
 
-    Set<String> computePermutation(String str) {
-        throw new UnsupportedOperationException();
+    public ArrayList<String> computePermutation(String str) {
+        if(str == null) return null;
+
+        ArrayList<String> permutations = new ArrayList<>();
+
+        if(str.length() == 0) {
+            permutations.add("");
+            return permutations;
+        }
+
+        var first = str.charAt(0);
+        var remainder = str.substring(1);
+
+        ArrayList<String> words = computePermutation(remainder);
+
+        for(String word : words) {
+            for(int j = 0; j <= word.length(); j++) {
+                String s = InsertCharAt(word, first, j);
+                permutations.add(s);
+            }
+        }
+        return permutations;
+    }
+
+    private String InsertCharAt(String word, char c, int i) {
+        String start = word.substring(0, i);
+        String end = word.substring(i);
+        return start + c + end;
     }
 
 }
