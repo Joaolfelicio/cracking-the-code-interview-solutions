c8d6330befe164ee8ebfeddfbde6eb005ed30980
Joao <joaolfelicio@gmail.com>
2021-01-08 18:49:05 +0100

    Add Permutations with Duplication Solution

--- Algorithms/src/main/java/recursivedp/_08_08_PermutationWithDup.java
+++ Algorithms/src/main/java/recursivedp/_08_08_PermutationWithDup.java
@@ -1,5 +1,7 @@
 package recursivedp;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Set;
 
 /**
@@ -8,8 +10,39 @@ import java.util.Set;
  */
 class _08_08_PermutationWithDup {
 
-    Set<String> computePermutation(String str) {
-        throw new UnsupportedOperationException();
+    public ArrayList<String> printPerms(String s) {
+        ArrayList<String> result = new ArrayList<>();
+        HashMap<Character, Integer> map = buildFreqTable(s);
+        printPerms(map, "", s.length(), result);
+        return result;
+    }
+
+    private HashMap<Character, Integer> buildFreqTable(String s) {
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        for(char c : s.toCharArray()) {
+            if(!map.containsKey(c)) {
+                map.put(c, 0);
+            }
+            map.put(c, map.get(c) + 1);
+        }
+        return map;
+    }
+
+    private void printPerms(HashMap<Character, Integer> map, String prefix, int remaining, ArrayList<String> result) {
+        if(remaining == 0) {
+            result.add(prefix);
+            return;
+        }
+
+        for(Character c : map.keySet()) {
+            int count = map.get(c);
+            if(count > 0) {
+                map.put(c, count - 1);
+                printPerms(map, prefix + c, remaining - 1, result);
+                map.put(c, count);
+            }
+        }
     }
 
 }
